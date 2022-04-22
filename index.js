59caeee64e3d2bf7afe0417e308a9c2706bfe80f
Berfin <berfin@minerva.kgi.edu>
2022-04-22 16:08:25 +0300

    hii i ma done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+
+function superbowlWin(a) {
+    const result = a.find((y) => y.result === "W");
+    if (result) {
+        return result.year;
+        
+    } else {
+        return undefined;
+
+    }
+}
